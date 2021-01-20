59cbf5dc2e8e0a59cf8110e1286310111cfadaa7
MDC <markdcross@gmail.com>
2021-01-20 17:29:34 -0500

    Updating and deleting bootcamps -PUT & DELETE

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,9 @@ exports.getBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
 
-    res.status(200).json({ success: true, data: bootcamps });
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (err) {
     res.status(400).json({ success: false });
   }
@@ -57,10 +59,21 @@ exports.createBootcamp = async (req, res, next) => {
 //!   @desc     Update Bootcamp
 //*   @access   Private
 //* ======================================
-exports.updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //* ======================================
@@ -68,8 +81,16 @@ exports.updateBootcamp = (req, res, next) => {
 //!   @desc     Delete Bootcamp
 //*   @access   Private
 //* ======================================
-exports.deleteBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };

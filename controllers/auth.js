35eb30b1974a16c461fe14aae62a806827dc836e
MDC <markdcross@gmail.com>
2021-01-29 09:13:31 -0500

    Added cookie authentication (currently commented out)

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,6 +54,23 @@ exports.login = asyncHandler(async (req, res, next) => {
 });
 
 //* ===============================================================
+//*   @route    GET /api/v1/auth/logout
+//!   @desc     Log user out and clear cookie
+//*   @access   Private
+//* ===============================================================
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
+
+//* ===============================================================
 //*   @route    POST /api/v1/auth/me
 //!   @desc     Get current logged in user
 //*   @access   Private
